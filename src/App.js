19253ad826dad34de3465b67457ed9979e50f35c
0seo8 <0seo23@gmail.com>
2022-05-15 13:47:07 +0900

    webapck-template

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import './stylesheet/main.scss';
+import imgSrc from '../static/image__header-search.png'
+
+const imgEl = document.createElement('img')
+imgEl.src = imgSrc
+document.body.append(imgEl)
